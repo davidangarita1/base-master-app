3c0326bb5a66d42d3f52ee2a67d4b1e1e81b0dc4
davidangarita1 <davidangarita.sistemas@gmail.com>
2022-03-03 17:50:26 -0500

    refactor de creator

--- Project/creator.sh
+++ Project/creator.sh
@@ -2,7 +2,7 @@
 
 COMM=$1;
 NAME=$2;
-SOURCE="https://raw.githubusercontent.com/davidangarita1/base-master-app/main/src";
+SOURCE="https://raw.githubusercontent.com/davidangarita1/base-master-app/main/Project/base/redux";
 
 if [ "$COMM" = "redux" ]; then
 	NAMEU=${NAME^^}
@@ -15,29 +15,33 @@ if [ "$COMM" = "redux" ]; then
 	# Redux
 		cd src/redux/
 		# Getting the action templates
-			cd actions/actionTypes
-				curl -o actionTypes$NAMECamel.js $SOURCE/redux/actions/actionTypes/ActionTypesExample.js
-				sed -i 's/EXAMPLE/'$NAMEU'/g' actionTypes$NAMECamel.js
-				sed -i 's/Example/'$NAMECamel'/g' actionTypes$NAMECamel.js
-			cd ..
-				curl -o $NAMEL'Actions.js' $SOURCE/redux/actions/exampleActions.js
-				sed -i 's/EXAMPLE/'$NAMEU'/g' $NAMEL'Actions.js'
-				sed -i 's/Example/'$NAMECamel'/g' $NAMEL'Actions.js'
-				sed -i 's/example/'$NAMEL'/g' $NAMEL'Actions.js'
-			cd .. && cd reducers
-				curl -o $NAMEL'Reducer.js' $SOURCE/redux/reducers/ExampleReducer.js
-				sed -i 's/EXAMPLE/'$NAMEU'/g' $NAMEL'Reducer.js'
-				sed -i 's/Example/'$NAMECamel'/g' $NAMEL'Reducer.js'
-				sed -i 's/example/'$NAMEL'/g' $NAMEL'Reducer.js'
-			cd .. && cd middlewares
-				curl -o $NAMEL'Middleware.js' $SOURCE/redux/middlewares/exampleMiddleware.js
-				sed -i 's/EXAMPLE/'$NAMEU'/g' $NAMEL'Middleware.js'
-				sed -i 's/Example/'$NAMECamel'/g' $NAMEL'Middleware.js'
-				sed -i 's/example/'$NAMEL'/g' $NAMEL'Middleware.js'
-			cd .. && cd app
-				sed -i "s/'redux'/'redux'\nimport "$NAMEL"Reducer from '..\/reducers\/"$NAMEL"Reducer'/g" rootReducer.js
-				sed -i 's/combineReducers({/combineReducers({\n\t\t  '$NAMEL': '$NAMEL'Reducer,/g' rootReducer.js
-			cd
+		cd actions/actionTypes
+			curl -o actionTypes$NAMECamel.js $SOURCE/actions/actionTypes/actionTypesExample.js
+			sed -i 's/EXAMPLE/'$NAMEU'/g' actionTypes$NAMECamel.js
+			sed -i 's/Example/'$NAMECamel'/g' actionTypes$NAMECamel.js
+		cd ..
+		# Getting the Action templates
+			curl -o $NAMEL'Actions.js' $SOURCE/actions/exampleActions.js
+			sed -i 's/EXAMPLE/'$NAMEU'/g' $NAMEL'Actions.js'
+			sed -i 's/Example/'$NAMECamel'/g' $NAMEL'Actions.js'
+			sed -i 's/example/'$NAMEL'/g' $NAMEL'Actions.js'
+		cd .. && cd reducers
+		# Getting the reducer templates
+			curl -o $NAMEL'Reducer.js' $SOURCE/reducers/exampleReducer.js
+			sed -i 's/EXAMPLE/'$NAMEU'/g' $NAMEL'Reducer.js'
+			sed -i 's/Example/'$NAMECamel'/g' $NAMEL'Reducer.js'
+			sed -i 's/example/'$NAMEL'/g' $NAMEL'Reducer.js'
+		cd .. && cd middlewares
+		# Getting the middleware templates
+			curl -o $NAMEL'Middleware.js' $SOURCE/middlewares/exampleMiddleware.js
+			sed -i 's/EXAMPLE/'$NAMEU'/g' $NAMEL'Middleware.js'
+			sed -i 's/Example/'$NAMECamel'/g' $NAMEL'Middleware.js'
+			sed -i 's/example/'$NAMEL'/g' $NAMEL'Middleware.js'
+		cd .. && cd app
+		# Getting the app templates
+			sed -i "s/'redux'/'redux'\nimport "$NAMEL"Reducer from '..\/reducers\/"$NAMEL"Reducer'/g" rootReducer.js
+			sed -i 's/combineReducers({/combineReducers({\n\t\t  '$NAMEL': '$NAMEL'Reducer,/g' rootReducer.js
+		cd ..
 	echo "------------------------------------------------------"
-	echo $NAMECamel"Reducer Created 😀";
+	echo $NAMECamel"Reducer Created";
 fi
