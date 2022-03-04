06cf7769c27cc7439ffd563a4903861e477a3f3e
davidangarita1 <davidangarita.sistemas@gmail.com>
2022-03-03 19:58:38 -0500

    Se corrige bug

--- Project/base/main.jsx
+++ Project/base/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
-import configureStore from '../redux/app/store'
+import configureStore from './redux/app/store'
 
 const store = configureStore()
 
